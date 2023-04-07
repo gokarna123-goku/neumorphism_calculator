4e5b51cef979960b20d493930fbd47e75956e0b7
gokarna123-goku <agokarna444@gmail.com>
2023-04-07 19:48:12 +0545

    changes

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ function clearInput() {
   input.value = "";
 }
 
-// 
+// backspace removes the last character from the input field
 function backspace() {
   input.value = input.value.slice(0, -1);
 }
 
+// calculate evaluates the input expression using the eval function 
+// and displays the result in the input field. If there is an error, it displays "Error" instead.
 function calculate() {
   try {
     input.value = eval(input.value);
@@ -18,20 +20,26 @@ function calculate() {
   }
 }
 
+// inputNum appends the given number to the input field
 function inputNum(num) {
   input.value += num;
 }
 
+// inputOperator appends the given operator to the input field
 function inputOperator(operator) {
   input.value += operator;
 }
 
+
+// inputDecimal appends a decimal point to the input field if there isn't already one
 function inputDecimal() {
   if (!input.value.includes(".")) {
     input.value += ".";
   }
 }
 
+
+// inputNegative toggles the sign of the input field
 function inputNegative() {
   if (input.value[0] !== "-") {
     input.value = "-" + input.value;
@@ -39,3 +47,5 @@ function inputNegative() {
     input.value = input.value.slice(1);
   }
 }
+
+// End 
